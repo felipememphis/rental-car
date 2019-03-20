338bc48fbe99dc03452a570f02c446b508ba7ede
Felipe Lemos <felipe.lemos@zup.com.br>
2019-03-20 11:03:50 -0300

    setting swagger and documentation api client

--- rental-car/src/main/java/com/rentalcar/controller/ClientController.java
+++ rental-car/src/main/java/com/rentalcar/controller/ClientController.java
@@ -1,5 +1,7 @@
 package com.rentalcar.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,15 +21,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rentalcar.entity.Client;
 import com.rentalcar.service.ClientService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/client")
+@Api(value="Api rest for client")
 public class ClientController {
 
 	@Autowired
 	private ClientService service;
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> findByName(@RequestParam String name){
+	@ApiOperation(value="Busca o cliente pelo seu nome", response = List.class)
+	@ApiResponses(value = {
+			@ApiResponse(code=200, message="Sucesso ao buscar cliente por nome."),
+			@ApiResponse(code=404, message="Cliente não encontrado!"),
+	})
+	public ResponseEntity<?> findByName(@ApiParam(value="Nome do cliente a ser pesquisado", required=true)@RequestParam String name){
 		Client client;
 		try {
 			client = service.findByName(name);
@@ -39,7 +53,12 @@ public class ClientController {
 	}
 	
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,  consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> save(@Valid @RequestBody Client client) {
+	@ApiOperation(value="Salva o cliente", response = List.class)
+	@ApiResponses(value = {
+			@ApiResponse(code=201, message="Sucesso ao salvar o cliente."),
+			@ApiResponse(code=400, message="Cliente não é maior de 18 anos!"),
+	})
+	public ResponseEntity<?> save(@ApiParam(value="Dados do cliente a ser salvo", required=true)@Valid @RequestBody Client client) {
 		Client result = null;
 		try {
 			result = service.save(client);
@@ -50,18 +69,28 @@ public class ClientController {
 	}
 	
 	@PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE,  consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> update(@PathVariable Long id, @Valid @RequestBody Client client) {
+	@ApiOperation(value="Atualiza o cliente", response = List.class)
+	@ApiResponses(value = {
+			@ApiResponse(code=200, message="Sucesso ao salvar o cliente."),
+			@ApiResponse(code=404, message="Cliente não encontrado!"),
+	})
+	public ResponseEntity<?> update(@ApiParam(value="Dados do cliente a ser atualizado", required=true)@PathVariable Long id, @Valid @RequestBody Client client) {
 		Client result = null;
 		try {
 			result = service.update(client, id);
 		} catch (Exception e) {
 			return new ResponseEntity<>("cliente não encontrado", HttpStatus.NOT_FOUND);
 		}
-		return new ResponseEntity<>(result, HttpStatus.CREATED);
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> remove(@PathVariable Long id) {
+	@ApiOperation(value="Remove o cliente", response = List.class)
+	@ApiResponses(value = {
+			@ApiResponse(code=204, message="Sucesso ao excluir o cliente."),
+			@ApiResponse(code=404, message="Cliente não encontrado!"),
+	})
+	public ResponseEntity<?> remove(@ApiParam(value="Id do cliente a ser removido", required=true)@PathVariable Long id) {
 		try {
 			service.remove(id);
 		} catch (Exception e) {
